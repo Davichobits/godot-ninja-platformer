078b1f252b1c7a6ec899fe24b1d91c7707de6bda
David Ruiz <davidruizdeveloper@gmail.com>
2026-08-25 05:37:45 -0500

    basic player collisions

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,6 @@ func _physics_process(delta: float) -> void:
 		#velocity.x = 50
 	#if Input.is_action_pressed("ui_left"):
 		#velocity.x = -50
-	var x_input = Input.get_axis("ui_left", "ui_right")
+	var x_input = Input.get_axis("left", "right")
 	velocity.x = x_input * 50
 	move_and_slide()
