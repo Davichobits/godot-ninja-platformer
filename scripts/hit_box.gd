e5b1ac70f1b66d4e41276a320110aee994a869cb
David Ruiz <davidruizdeveloper@gmail.com>
2026-09-17 18:05:45 -0500

    Add cannon enemy and combat hitboxes
    This commit introduces a basic cannon enemy scene along with shared Hurtbox/Hitbox scripts for interaction checks. It also updates the world scene to include the cannon and sets the default background to black. Player movement tuning was adjusted to make acceleration and friction more responsive.

---
+++ scripts/hit_box.gd
@@ -0,0 +1,14 @@
+class_name Hitbox extends Area2D
+
+@export var damage: = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	pass # Replace with function body.
+
+func _on_area_entered(area_2d: Area2D) -> void:
+	assert(area_2d is Hurtbox, "The hitbox detected an area that wasn't a hurtbox")
+	var hurtbox = area_2d as Hurtbox
+	if hurtbox.is_invincible: return
+	hurtbox.hurt.emit(self)
